e0b4050d89f16274c31b2424ed9f1f1b76fa6859
James Lee <egypt@metasploit.com>
2014-12-04 11:48:18 -0600

    Whitespace

--- lib/net/ntlm/blob.rb
+++ lib/net/ntlm/blob.rb
@@ -4,13 +4,13 @@ module Net
     BLOB_SIGN = 0x00000101
 
     class Blob < FieldSet
-      int32LE    :blob_signature,   {:value => BLOB_SIGN}
-      int32LE    :reserved,         {:value => 0}
+      int32LE    :blob_signature, {:value => BLOB_SIGN}
+      int32LE    :reserved,       {:value => 0}
       int64LE    :timestamp,      {:value => 0}
       string     :challenge,      {:value => "", :size => 8}
-      int32LE    :unknown1,     {:value => 0}
-      string     :target_info,      {:value => "", :size => 0}
-      int32LE    :unknown2,         {:value => 0}
+      int32LE    :unknown1,       {:value => 0}
+      string     :target_info,    {:value => "", :size => 0}
+      int32LE    :unknown2,       {:value => 0}
     end
 
   end
