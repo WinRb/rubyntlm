e7544b91ce709209e331af822932beb0c2559550
James Lee <egypt@metasploit.com>
2015-02-09 22:32:58 -0600

    Document arguments for the constructor

--- lib/net/ntlm/client.rb
+++ lib/net/ntlm/client.rb
@@ -7,11 +7,25 @@ module Net
         NTLM::FLAGS[:NTLM]   | NTLM::FLAGS[:ALWAYS_SIGN]  | NTLM::FLAGS[:NTLM2_KEY] |
         NTLM::FLAGS[:KEY128] | NTLM::FLAGS[:KEY_EXCHANGE] | NTLM::FLAGS[:KEY56]
 
-      attr_reader :username, :password, :domain, :workstation, :flags
+      attr_reader :username
+
+      attr_reader :password
+
+      attr_reader :domain
+
+      attr_reader :workstation
+
+      attr_reader :flags
 
       # @note All string parameters should be encoded in UTF-8. The proper
-      #   final encoding for placing in the various {Message}s will be chosen
-      #   based on negotiation with the server.
+      #   final encoding for placing in the various {Message messages} will be
+      #   chosen based on negotiation with the server.
+      #
+      # @param username [String]
+      # @param password [String]
+      # @option opts [String] :domain where we're authenticating to
+      # @option opts [String] :workstation local workstation name
+      # @option opts [Fixnum] :flags (DEFAULT_FLAGS) see Net::NTLM::Message::Type1.flag
       def initialize(username, password, opts = {})
         @username     = username
         @password     = password
@@ -20,6 +34,7 @@ module Net
         @flags        = opts[:flags] || DEFAULT_FLAGS
       end
 
+      # @return [NTLM::Message]
       def init_context(resp = nil)
         if resp.nil?
           @session = nil
@@ -30,19 +45,20 @@ module Net
         end
       end
 
+      # @return [Client::Session]
       def session
         @session
       end
 
-
       private
 
-
+      # @return [Message::Type1]
       def type1_message
         type1 = Message::Type1.new
         type1[:flag].value = flags
         type1.domain = domain if domain
         type1.workstation = workstation if workstation
+
         type1
       end
 
