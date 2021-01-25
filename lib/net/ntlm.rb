f04cff7c6499217b99f3d002f90c1a3aaec25969
lridge <lridge@igsenergy.com>
2021-01-25 08:43:09 -0500

    Update open ssl calls in des cyper application to follow the suggested spec laid out by the openssl gem

--- lib/net/ntlm.rb
+++ lib/net/ntlm.rb
@@ -123,11 +123,11 @@ module Net
       end
 
       def apply_des(plain, keys)
-        dec = OpenSSL::Cipher.new("des-cbc")
+        dec = OpenSSL::Cipher.new("des-cbc").encrypt
         dec.padding = 0
         keys.map {|k|
           dec.key = k
-          dec.encrypt.update(plain) + dec.final
+          dec.update(plain) + dec.final
         }
       end
 
